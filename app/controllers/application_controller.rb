8a86eccd2e2564bdaf97f1a343154d917babad4a
Chris Peak <christopeak@gmail.com>
2014-12-09 06:32:24 -0800

    partway through adding current time to sidebar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+	
+	helper_method :time #added this in an attempt to make @time accessible from applicaiton.html.erb.
+	
+	def time
+		@time = Time.now
+	end
 end
